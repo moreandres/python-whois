8361497c3d85e0a18531623712edb785b8338578
Andres More <moreandres@users.noreply.github.com>
2024-03-26 09:05:11 -0300

    Update parser.py

--- whois/parser.py
+++ whois/parser.py
@@ -2956,7 +2956,7 @@ class WhoisTr(WhoisEntry):
         'creation_date': r'Created on.*: *(.+)',
         'expiration_date': r'Expires on.*: *(.+)',
 
-        'name_servers': r'^([a-z0-9]\S+)\s+\S+$',
+        'name_servers': r'^([a-z0-9].[a-z0-9.]+)$',
 
         'registrant_name': r'(?<=[**] Registrant:)[\s\S]((?:\s.+)*)',
 
