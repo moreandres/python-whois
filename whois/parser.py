4475d51e49209a258f49a670ab85b144af2f622f
Andres More <moreandres@users.noreply.github.com>
2024-04-02 12:53:47 -0300

    Update parser.py

--- whois/parser.py
+++ whois/parser.py
@@ -3972,7 +3972,7 @@ class WhoisSi(WhoisEntry):
     regex = {
         'domain_name': r'domain: *(.+)',
         'registrar': r'registrar: *(.+)',
-        'name_servers': r'^nameserver:[ ]+(\S+)$',
+        'name_servers': r'^nameserver:[ \t]+(\S+)\s*\S*$',
         'registrant_name': r'registrant: *(.+)',
         'creation_date': r'created: *(.+)',
         'expiration_date': r'expire: *(.+)',
