68087fdb7654a87924c53b67da8ca0d16e81221d
Andres More <moreandres@users.noreply.github.com>
2024-04-03 09:24:10 -0300

    Update parser.py

--- whois/parser.py
+++ whois/parser.py
@@ -5854,7 +5854,7 @@ class WhoisMd(WhoisEntry):
         'domain_status': r'Domain  state: *(.+)',
         'creation_date': r'Registered on: *(.+)',
         'expiration_date': r'Expires    on: *(.+)',
-        'name_servers': r'Nameserver: *(.+)',
+        'name_servers': r'^Nameserver:[ ]+(\S+)$',
     }
 
     def __init__(self, domain, text):
