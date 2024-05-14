b09c77536c75a3987c6361f07c2b92dfce5ed0fd
Andres More <moreandres@users.noreply.github.com>
2024-05-14 15:48:48 -0300

    Update parser.py

--- whois/parser.py
+++ whois/parser.py
@@ -4950,7 +4950,7 @@ class WhoisCi(WhoisEntry):
     }
 
     def __init__(self, domain, text):
-        if 'Not found:' in text:
+        if 'Not found:' in text or 'Domain Status: No Object Found' in text:
             raise PywhoisError(text)
         else:
             WhoisEntry.__init__(self, domain, text, self.regex)
